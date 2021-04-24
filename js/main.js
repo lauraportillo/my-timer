2e11af0f43e5be81da585ac6d0329c307c2cac60
Laura Portillo <lauraportillo.ark@gmail.com>
2021-04-24 14:47:56 +0200

    work timer properly

--- js/main.js
+++ js/main.js
@@ -16,48 +16,47 @@ let interval;
 
 function handleStartBtn(evt) {
   evt.preventDefault();
-  const hours = parseInt(hoursElement.value);
-  const minutes = parseInt(minutesElement.value);
-  const seconds = parseInt(secondsElement.value);
-  counterSeconds = seconds;
-  counterMinutes = minutes;
-  counterHours = hours;
-  paintTimer(hours, minutes, seconds);
+  counterHours = parseInt(hoursElement.value);
+  counterMinutes = parseInt(minutesElement.value);
+  counterSeconds = parseInt(secondsElement.value);
 
   if (counterHours !== 0 || counterMinutes !== 0 || counterSeconds !== 0) {
     // 1000 milisegundo se trabaja en informática equivale a cada 1 segundo
-    interval = setInterval(handleCounter, 1000, hours, minutes, seconds);
+    interval = setInterval(handleCounter, 1000);
   }
+  endElement.innerHTML = '';
+  paintTimer(counterHours, counterMinutes, counterSeconds);
 }
 
 function handleCounter() {
   counterSeconds -= 1;
-  if (counterSeconds === -1 && counterMinutes !== 0) {
-    counterMinutes -= 1;
+
+  if (counterSeconds === -1) {
     counterSeconds = 59;
-  }
-  if (counterMinutes === -1 && counterHours !== 0) {
-    counterHours -= 1;
-    counterMinutes = 59;
+    counterMinutes -= 1;
+    if (counterMinutes === -1) {
+      counterMinutes = 59;
+      counterHours -= 1;
+    }
   }
 
-  console.log(counterHours, counterMinutes, counterSeconds);
   if (counterSeconds === 0 && counterMinutes === 0 && counterHours === 0) {
     clearInterval(interval); // sirve para parar el tiempo
     endElement.innerHTML = 'time is over';
-    //esta condición no está funcionando
   }
   paintTimer(counterHours, counterMinutes, counterSeconds);
 }
 
 function handlePauseBtn(evt) {
-  //añadiendo prevent default hemos conseguido que cuando pulso el botón se para el tiempo donde esté
   evt.preventDefault();
   clearInterval(interval);
 }
 
-function handleStopBtn() {
+function handleStopBtn(evt) {
+  evt.preventDefault();
   clearInterval(interval);
+  paintTimer(0, 0, 0);
+  endElement.innerHTML = '';
 }
 
 function paintTimer(hours, minutes, seconds) {
@@ -79,6 +78,3 @@ function paintTimer(hours, minutes, seconds) {
 buttonStartElement.addEventListener('click', handleStartBtn);
 buttonPauseElement.addEventListener('click', handlePauseBtn);
 buttonStopElement.addEventListener('click', handleStopBtn);
-
-// cuando llegue a 0 minutos  0 seg tiene que sonar la bocina
-// investigar cómo parar el set interval
